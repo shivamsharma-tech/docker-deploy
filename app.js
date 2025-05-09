430281df4e4a585fc5608896f42886881c8e56f8
Shivam Sharma <shivamsharma3835@gmail.com>
2025-05-09 21:43:48 +0530

    jk

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send(`Running on port ${port}`);
 });
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`Example app listening on port ${port}`);
 });
