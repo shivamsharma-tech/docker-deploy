edc62c747ece2ec86dd6ee133430397d855ebc3b
Shivam Sharma <shivamsharma3835@gmail.com>
2025-05-10 11:39:54 +0530

    yj

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-  res.send(`Running on port ${port}`);
+  res.send(`This page comes from git push -> jenkins -> create image -> deploye to  aws -> start container in port ${port}`);
 });
 
 app.listen(port,'0.0.0.0', () => {
